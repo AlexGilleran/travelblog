5ecca5d820cd684438bdb0c347f63a312db8dd81
Alex Gilleran <alex@alexgilleran.com>
2015-04-04 23:18:55 +1100

    Made it return the text of the first row.

--- src/main/scala/com/alexgilleran/travelblog/MyService.scala
+++ src/main/scala/com/alexgilleran/travelblog/MyService.scala
@@ -1,9 +1,11 @@
 package com.alexgilleran.travelblog
 
 import akka.actor.Actor
+import com.alexgilleran.travelblog.data.schema.Tables
 import spray.http.MediaTypes._
 import spray.http._
 import spray.routing._
+import Tables.profile.simple._
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -23,17 +25,24 @@ class MyServiceActor extends Actor with MyService {
 // this trait defines our service behavior independently from the service actor
 trait MyService extends HttpService {
 
+  val thing : TableQuery[Tables.Entry] = Tables.Entry
+  val db = Database.forURL("jdbc:postgresql:TravelBlog?user=postgres&password=p4ssw0rd", driver = "org.postgresql.Driver")
+
+
+
   val myRoute =
     pathPrefix("blah" / Segment) { stringArg =>
       pathEnd {
         get {
           respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
             complete {
-              <html>
-                <body>
-                  <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!{stringArg}</h1>
-                </body>
-              </html>
+              db.withSession { implicit session =>
+                <html>
+                  <body>
+                    <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!{ Tables.Entry.first.text}</h1>
+                  </body>
+                </html>
+              }
             }
           }
         }
