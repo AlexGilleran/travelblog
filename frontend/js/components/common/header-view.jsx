a91574647f7ea561d6587cc5cd3ac4f5c32291ca
Alex Gilleran <alex.gilleran@valtech.com>
2015-07-01 22:14:49 -0400

    Made header link to home.

--- frontend/js/components/common/header-view.jsx
+++ frontend/js/components/common/header-view.jsx
@@ -1,12 +1,15 @@
 var React = require('react');
 import LoginView from './login-view';
+import {Link} from 'react-router';
 
 export default class HeaderView extends React.Component {
   render() {
     return (
       <div className="col-1-1">
         <div className="content pull-left">
-          <h1>Blog</h1>
+          <Link to="app">
+            <h1>Blog</h1>
+          </Link>
         </div>
         <div className="pull-right">
           <LoginView flux={this.props.flux} />
