e522b1228edcc68e069d514faad03d3bc410cc8b
Alex Gilleran <alex.gilleran@valtech.com>
2015-07-19 14:08:23 -0400

    Giving up on local docker.

--- run-docker-locally.sh
+++ run-docker-locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-IP=$(boot2docker ip)
-export STATIC_ASSET_BASE=http://$IP:2992/assets/
+export IP=$(boot2docker ip)
+export STATIC_ASSET_BASE=http://$IP:2992/_assets/
 export AJAX_BASE=http://$IP/api/
 
 #docker-compose -f docker-compose-local.yml build
