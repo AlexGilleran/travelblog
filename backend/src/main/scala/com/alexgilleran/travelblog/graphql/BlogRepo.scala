008677fb40f799c3d8fb108a2769539951265448
Alex Gilleran <alex@alexgilleran.com>
2016-05-29 16:39:24 +0000

    Created graphql link from blog -> entries

--- backend/src/main/scala/com/alexgilleran/travelblog/graphql/BlogRepo.scala
+++ backend/src/main/scala/com/alexgilleran/travelblog/graphql/BlogRepo.scala
@@ -4,21 +4,24 @@ import sangria.schema.Deferred
 import sangria.schema.DeferredResolver
 import scala.util.Try
 import scala.concurrent.Future
-import com.alexgilleran.travelblog.data.schema.Tables.Entry
+import com.alexgilleran.travelblog.data.schema.Tables._
 import com.alexgilleran.travelblog.data.GeneralDAO
 import com.alexgilleran.travelblog.data.PostGresSlickDAO
 
-case class DeferEntries(entryIds: Seq[Long]) extends Deferred[Seq[Entry]]
 
 class BlogRepo {
   val dao: GeneralDAO = PostGresSlickDAO
 
   def getEntries(entryIds: Seq[Long]): Future[Seq[Entry]] = dao.getEntries(entryIds)
+  def getEntriesForBlog(blogId: Long): Future[Seq[Entry]] = dao.getEntriesForBlog(blogId)
+  def getBlog(blogId: Long) : Future[Option[Blog]] = dao.getBlog(blogId)
 }
+
+case class DeferEntriesForBlog(blogId: Long) extends Deferred[Seq[Entry]]
 
 class BlogResolver extends DeferredResolver[BlogRepo] {
   override def resolve(deferred: Vector[Deferred[Any]], ctx: BlogRepo) = deferred.map {
-    case DeferEntries(entryIds) =>
-      ctx.getEntries(entryIds)
+    case DeferEntriesForBlog(blogId) =>
+      ctx.getEntriesForBlog(blogId)
   }
 }
--- backend/src/main/scala/com/alexgilleran/travelblog/graphql/SchemaDefinition.scala
+++ backend/src/main/scala/com/alexgilleran/travelblog/graphql/SchemaDefinition.scala
@@ -2,32 +2,49 @@ package com.alexgilleran.travelblog.graphql
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import com.alexgilleran.travelblog.data.schema.Tables.Blog
 import com.alexgilleran.travelblog.data.schema.Tables.Entry
 
 import sangria.schema.Action.defaultAction
+import sangria.schema.Action.deferredAction
 import sangria.schema.Action.futureAction
 import sangria.schema.Argument
 import sangria.schema.Field
+import sangria.schema.ListType
 import sangria.schema.LongType
 import sangria.schema.ObjectType
 import sangria.schema.OptionType
+import sangria.schema.Schema
 import sangria.schema.StringType
 import sangria.schema.fields
-import sangria.schema.Schema
 
 object SchemaDefinition {
+
   val EntryType: ObjectType[Unit, Entry] = ObjectType(
     "Entry",
     fields[Unit, Entry](
       Field("entryId", OptionType(LongType), resolve = _.value.entryId),
-      Field("markdown", OptionType(StringType), resolve = _.value.markdown),
+      Field("markdown", StringType, resolve = _.value.markdown),
       Field("title", OptionType(StringType), resolve = _.value.title),
-      Field("blogId", OptionType(LongType), resolve = _.value.blogId)))
+      Field("blogId", LongType, resolve = _.value.blogId)))
+
+  val BlogType: ObjectType[BlogRepo, Blog] = ObjectType(
+    "Blog",
+    fields[BlogRepo, Blog](
+      Field("blogId", OptionType(LongType), resolve = _.value.blogId),
+      Field("name", StringType, resolve = _.value.name),
+      Field("description", OptionType(StringType), resolve = _.value.description),
+      Field("userId", LongType, resolve = _.value.userId),
+      Field("entries", ListType(EntryType), resolve = (ctx) => DeferEntriesForBlog(ctx.value.blogId.get))))
 
   val EntryID = Argument("entryId", LongType, description = "id of the entry")
+  val BlogID = Argument("blogId", LongType, description = "id of the blog")
 
   val Query = ObjectType[BlogRepo, Unit](
     "Query", fields[BlogRepo, Unit](
+      Field("blog", OptionType(BlogType),
+        arguments = BlogID :: Nil,
+        resolve = (ctx) => ctx.ctx.getBlog(ctx.arg(BlogID))),
       Field("entry", EntryType,
         arguments = EntryID :: Nil,
         resolve = (ctx) => ctx.ctx.getEntries(Seq(ctx.arg(EntryID))).map(_.head))))
